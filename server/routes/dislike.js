7084b2370d11459029f9c6b23ecd05a03d3cf1a3
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-27 11:03:30 +0900

    directory name 변경

---
+++ server/routes/dislike.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { DisLike } = require("../models/DisLike");
+
+//=================================
+//             DisLike
+//=================================
+
+
+
+
+module.exports = router;
