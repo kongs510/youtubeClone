3064835f979d0700903a73c4e9fa20b951df9d0a
공민철 <68903200+kongs510@users.noreply.github.com>
2021-08-06 15:01:25 +0900

    Updates

---
+++ precache-manifest.c58c0ac8d93c2fd4b9ab99e61af962a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef518fb2c79554232471c4bc378e4bc3",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "a2bc5fe446e87babdd05",
+    "url": "/react-youtube/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "c939d89842c88573bca4",
+    "url": "/react-youtube/static/css/main.dccba91d.chunk.css"
+  },
+  {
+    "revision": "a2bc5fe446e87babdd05",
+    "url": "/react-youtube/static/js/2.6d2a9ea9.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/react-youtube/static/js/2.6d2a9ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c939d89842c88573bca4",
+    "url": "/react-youtube/static/js/main.90347e7d.chunk.js"
+  },
+  {
+    "revision": "b497fd9bb0c7fadf5316",
+    "url": "/react-youtube/static/js/runtime-main.253a16c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtubeClone/precache-manifest.ee41e06adc4884a691bf00e177f50038.js"
+  "/react-youtube/precache-manifest.c58c0ac8d93c2fd4b9ab99e61af962a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtubeClone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-youtube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
